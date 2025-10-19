98dde6a25cfb208d18503f587a861c7e69ea4443
Demetryj <sdm17@ukr.net>
2025-10-19 22:01:56 +0300

    add FAQ data

---
+++ src/data/faqData.js
@@ -0,0 +1,84 @@
+export const faqData = [
+  {
+    title: 'What is Surecede and how does it simplify placement?',
+    body: [
+      <p key="1">
+        {`SureCede's core mission is to solve key inefficiencies in the reinsurance market, such as
+        reliance on manual data entry, lack of real-time analytics, and slow communication. It uses
+        automated technology extract key data from documents, provide deep risk analysis, and
+        facilitate interactive quoting and placement.`}
+      </p>,
+    ],
+  },
+  {
+    title: 'How can reinsurances and cedants benefit from SureCede?',
+    body: [
+      <p key="2">
+        The marketplace feature within SureCede is designed to create a more efficient and targeted
+        connection between cedants and reinsurers, moving beyond the traditional, fragmented
+        methods. It benefits both parties by streamlining the submission and underwriting process.
+      </p>,
+      <p key="3">
+        {`Cedants can create a standardized, high-quality data submission. When you enter the "Deals"
+        marketplace, their risk profile is presented in a format that reinsurers can easily
+        understand and analyze.`}
+        <br />
+        This creates an environment where their submission is more likely to be considered because
+        it is already organized to meet industry standards. The platform essentially makes it easier
+        for cedants to get their risks in front of the right reinsurers who have a defined appetite
+        for those specific risks, increasing the likelihood of a successful and efficient placement.
+      </p>,
+      <p key="4">
+        The marketplace is a powerful tool for reinsurers too, primarily because it allows them to
+        access submissions that are pre-filtered to match their risk appetite.
+        <br />
+        {`Reinsurers can build a detailed profile that defines their specific underwriting guidelines
+        and risk appetite based on criteria such as line of business, product type, and geographical
+        region. When a cedant submits a risk, the platform can automatically match it to reinsurers
+        whose guidelines align with the submission. This dramatically increases the quality of risk
+        submissions that land on an underwriter's desk, as they are no longer sifting through
+        irrelevant deals. This targeted approach allows reinsurers to work more efficiently and
+        focus on evaluating risks that are genuinely a good fit for their portfolio.`}
+      </p>,
+    ],
+  },
+  {
+    title: 'How does SureCede reduce intermediary/distribution costs?',
+    body: [
+      <p key="5">
+        While current market intermediaries can increase your acquisition costs by 20%-25%, SureCede
+        steps in and reduces that by up to 80%. While giving you access to both facultative risk
+        submission flow and reinsurance capacity, placed directly on the platform.
+      </p>,
+    ],
+  },
+  {
+    title: 'What lines of business are supported?',
+    body: [
+      <p key="6">
+        We support all major lines of reinsurance business such as Energy, Property, Construction,
+        Financial Lines, Cyber, Marine Cargo, Political Violence & Terrorism, Specie, etc. For
+        further enquiries, please contact us.
+      </p>,
+    ],
+  },
+  {
+    title: 'What jurisdictions can SureCede transaction business in?',
+    body: [
+      <p key="7">
+        As a DIFC-registered and DFSA regulated entity, we can transact or place business in any
+        jurisdiction globally, barring any local restrictions that prohibit this.
+      </p>,
+    ],
+  },
+  {
+    title: 'How can you book a demo?',
+    body: [
+      <p key="8">
+        Booking a demo is simple. Click the “Book a Demo” button on our homepage or contact page,
+        choose a convenient time, and our team will guide you through the platform. You’ll see how
+        Surecede streamlines reinsurance workflows, reduces costs, and accelerates deal-making.
+      </p>,
+    ],
+  },
+];
---
+++ src/data/footerData.js
@@ -0,0 +1,22 @@
+export const rulesLinksData = [
+  { title: 'Terms Of Use', linkTo: '' },
+  { title: 'Privacy Policy', linkTo: '' },
+];
+
+export const productNavDataFooter = [
+  { title: 'Platform', linkTo: '/platform' },
+  { title: 'Book a Demo', linkTo: '' },
+  { title: 'Log In', linkTo: '' },
+];
+
+export const generalNavDataFooter = [
+  { title: 'Contact Us', linkTo: '/contact-us' },
+  { title: 'Book a Demo', linkTo: '/about-us' },
+  { title: 'Market Insights', linkTo: '/market-insights' },
+];
+
+export const socialMediaData = [
+  { title: 'Facebook', linkTo: 'https://www.facebook.com/' },
+  { title: 'LinkedIn', linkTo: 'https://www.linkedin.com/' },
+  { title: 'Instagram', linkTo: 'https://www.instagram.com/' },
+];
