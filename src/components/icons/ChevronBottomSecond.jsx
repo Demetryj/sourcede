1eba871c00d9f2f86e519985ecce07a47833fed9
Demetryj <sdm17@ukr.net>
2025-10-20 22:55:11 +0300

    add icon

---
+++ src/components/icons/ChevronBottomSecond.jsx
@@ -0,0 +1,21 @@
+export default function ChevronBottomSecond({ className, props }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      className={className}
+      {...props}
+    >
+      <path
+        d="M18 9C18 9 13.581 15 11.9999 15C10.4188 15 6 9 6 9"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
