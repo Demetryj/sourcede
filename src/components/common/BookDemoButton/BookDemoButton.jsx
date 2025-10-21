2c81adfdf598125479369df28a5855db48dcee54
Demetryj <sdm17@ukr.net>
2025-10-21 20:46:56 +0300

    Update BookDemoButton.jsx

--- src/components/common/BookDemoButton/BookDemoButton.jsx
+++ src/components/common/BookDemoButton/BookDemoButton.jsx
@@ -1,7 +1,5 @@
 import { PrimaryButton } from '@/components/common';
 
-import './BookDemoButton.scss';
-
 export default function BookDemoButton() {
   return (
     <PrimaryButton withIcon href={''}>
