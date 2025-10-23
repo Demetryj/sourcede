5be68734052631faa15b439d158f09417f166692
Demetryj <sdm17@ukr.net>
2025-10-23 21:11:15 +0300

    add icons

---
+++ src/components/icons/ChevronLeftSecond.jsx
@@ -0,0 +1,21 @@
+export default function ChevronLeftSecond({ className, props }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      className={className}
+      {...props}
+    >
+      <path
+        d="M15 6.00005C15 6.00005 9.00001 10.419 9 12.0001C8.99999 13.5812 15 18 15 18"
+        stroke="#22281E"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/components/icons/cardIcon/Security.jsx
@@ -0,0 +1,42 @@
+export default function Security({ className, props }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      className={className}
+      {...props}
+    >
+      <path
+        d="M23.9997 27.3337C25.1847 27.0674 26.0847 26.63 26.8114 25.9106C28.6663 24.0741 28.6663 21.1184 28.6663 15.207C28.6663 9.29559 28.6663 6.33987 26.8114 4.50343C24.9563 2.66699 21.9707 2.66699 15.9997 2.66699C10.0286 2.66699 7.04299 2.66699 5.18799 4.50343C3.33301 6.33987 3.33301 9.29559 3.33301 15.207C3.33301 21.1184 3.33301 24.0741 5.18799 25.9106C5.91459 26.63 6.81466 27.0674 7.99967 27.3337"
+        stroke="#22281E"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+      <path d="M3.33301 11.333H28.6663" stroke="#22281E" strokeWidth="1.5" strokeLinejoin="round" />
+      <path
+        d="M9.33301 7.33301H9.34498"
+        stroke="#22281E"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.666 7.33301H14.678"
+        stroke="#22281E"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M20.6663 23.3337V20.0003C17.9997 20.0003 15.9997 18.667 15.9997 18.667C15.9997 18.667 13.9997 20.0003 11.333 20.0003V23.3337C11.333 28.0003 15.9997 29.3337 15.9997 29.3337C15.9997 29.3337 20.6663 28.0003 20.6663 23.3337Z"
+        stroke="#22281E"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
