cfeca43b27c09a2854c4d69c953f5098b18671bf
Demetryj <sdm17@ukr.net>
2025-10-23 21:59:19 +0300

    add

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,36 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import {
+  PrimaryButton,
+  SecondaryButton,
+  TertiaryButton,
+  TextButton,
+  SubscriptionForm,
+  InputGeneral,
+} from '@/components/common';
+import { HomePage } from '@/views';
+
 export default function Home() {
-  return <h1>kjshkjashkfjshdkfsjd</h1>;
+  useEffect(() => {
+    document.title = 'Home';
+  });
+
+  return (
+    <>
+      <div>
+        <h2>asdasdasdad</h2>
+        <p>ashdkasdkjahksjdhaksjdhaks</p>
+        <TextButton>Platform</TextButton>
+        <TertiaryButton>Book a Demo</TertiaryButton>
+        <SecondaryButton>Book a Dem</SecondaryButton>
+        <PrimaryButton withIcon>Book a Demo</PrimaryButton>
+        <SubscriptionForm />
+        <InputGeneral name="sdsfsdf" />
+      </div>
+
+      <HomePage />
+    </>
+  );
 }
