9f92f43f95e355b7a80c27a60bf2d5c7bdf24a37
Demetryj <sdm17@ukr.net>
2025-10-23 21:21:27 +0300

    add data for the About us page

---
+++ src/data/aboutUsPage.js
@@ -0,0 +1,111 @@
+import { Innovation, Vision, Security, Experts, Region } from '@/components/icons/cardIcon';
+
+export const numberedCardsDataAboutUsPage = [
+  {
+    number: '01',
+    title: 'Simplicity',
+    text: 'We designed SureCede to make insurance and reinsurance processes clear and intuitive.',
+  },
+  {
+    number: '02',
+    title: 'Efficiency',
+    text: 'We designed SureCede to help save time and improve margins for market participants.',
+  },
+  {
+    number: '03',
+    title: 'Transparency',
+    text: 'We designed SureCede to ensure users can access information clearly and effortlessly.',
+  },
+];
+
+export const advantagesCardListData = [
+  {
+    icon: Innovation,
+    title: 'Innovation',
+    text: 'Harnessing data analytics, AI, and cutting-edge APIs, we turn insights into action—driving smarter decisions and measurable outcomes.',
+  },
+  {
+    icon: Vision,
+    title: 'Market Leadership & Vision',
+    text: 'We aim to redefine reinsurance by combining expertise, technology, and insight to create a transparent, efficient, and future-ready ecosystem.',
+  },
+  {
+    icon: Security,
+    title: 'Security & Trust',
+    text: 'We uphold the highest standards of compliance and security, ensuring every engagement is anchored in integrity and reliability.',
+  },
+  {
+    icon: Experts,
+    title: 'Specialized Expertise',
+    text: 'From facultative to treaty reinsurance, our team has managed diverse portfolios, enabling us to tailor strategies that align with each client’s unique needs.',
+  },
+  {
+    icon: Region,
+    title: 'Trusted Expertise & Regional Presence',
+    text: 'With decades of combined industry experience, we have built strong relationships across MENA and DIFC. Our team brings deep local knowledge and global perspective to every partnership, ensuring strategies are effective and market-informed.',
+  },
+];
+
+export const teamListData = [
+  {
+    name: 'Andrew Johnson',
+    position: 'Chief Executive Officer',
+    linkToProfile: 'https://www.linkedin.com/',
+    image: '/images/team/andrew_johnson.png',
+  },
+  {
+    name: 'Evan Thompson',
+    position: 'Marketing Director',
+    linkToProfile: 'https://www.linkedin.com/',
+    image: '/images/team/evan_thompson.png',
+  },
+  {
+    name: 'Michael Smith',
+    position: 'Chief Technology Officer',
+    linkToProfile: 'https://www.linkedin.com/',
+    image: '/images/team//michael_smith.png',
+  },
+  {
+    name: 'Jan Chen',
+    position: 'Head of Product Development',
+    linkToProfile: 'https://www.linkedin.com/',
+    image: '/images/team/jan_chen.png',
+  },
+  {
+    name: 'David Lee',
+    position: 'Senior Data Analyst',
+    linkToProfile: 'https://www.linkedin.com/',
+    image: '/images/team//david_lee.png',
+  },
+  // duplication
+  {
+    name: 'Andrew Johnson',
+    position: 'Chief Executive Officer',
+    linkToProfile: 'https://www.linkedin.com/',
+    image: '/images/team/andrew_johnson.png',
+  },
+  {
+    name: 'Evan Thompson',
+    position: 'Marketing Director',
+    linkToProfile: 'https://www.linkedin.com/',
+    image: '/images/team/evan_thompson.png',
+  },
+  {
+    name: 'Michael Smith',
+    position: 'Chief Technology Officer',
+    linkToProfile: 'https://www.linkedin.com/',
+    image: '/images/team//michael_smith.png',
+  },
+  {
+    name: 'Jan Chen',
+    position: 'Head of Product Development',
+    linkToProfile: 'https://www.linkedin.com/',
+    image: '/images/team/jan_chen.png',
+  },
+  {
+    name: 'David Lee',
+    position: 'Senior Data Analyst',
+    linkToProfile: 'https://www.linkedin.com/',
+    image: '/images/team//david_lee.png',
+  },
+];
