827a7e00085a87f306f4ec3d8b6a9d259b5d8abb
Demetryj <sdm17@ukr.net>
2025-10-25 00:52:59 +0300

    add coverage, advantages section, results and nottom sections on the Home page

---
+++ src/data/homePageData.js
@@ -0,0 +1,176 @@
+import {
+  Dashboard,
+  Managment,
+  Terms,
+  Money,
+  Dynamic,
+  Tracking,
+  Distribution,
+  Coverage,
+} from '@/components/icons/cardIcon';
+
+import { SilverOakLogo, NovaRELogo, ApexRiskLogo } from '@/components/icons/companyLogo';
+
+export const coverageDataCardList = [
+  {
+    title: 'Financial Lines',
+    bage1: 'D&O',
+    bage2: 'PI',
+    image: '/images/coverage/financial-lines.webp',
+  },
+  {
+    title: 'Energy',
+    bage1: 'Downstream',
+    bage2: 'PI',
+    image: '/images/coverage/energy.webp',
+  },
+  {
+    title: 'Financial Lines',
+    bage1: 'D&O',
+    bage2: 'PI',
+    image: '/images/coverage/financiak-lines-2.webp',
+  },
+  {
+    title: 'Cyber',
+    bage1: 'Data',
+    bage2: 'Liability',
+    image: '/images/coverage/cyber.webp',
+  },
+  {
+    title: 'Construction',
+    bage1: 'CAR',
+    bage2: 'EAR',
+    image: '/images/coverage/construction.webp',
+  },
+  {
+    title: 'Heavy Industries',
+    bage1: 'Machinery',
+    bage2: 'BI',
+    image: '/images/coverage/heavy-industries.webp',
+  },
+  {
+    title: 'Marine Cargo',
+    bage1: 'Transit',
+    bage2: 'Goods',
+    image: '/images/coverage/marine-cargo.webp',
+  },
+  {
+    title: 'Specie Insurance',
+    bage1: 'Valuables',
+    bage2: 'Art',
+    image: '/images/coverage/specie-insurance.webp',
+  },
+  {
+    title: 'General Lines',
+    bage1: 'Product',
+    bage2: 'Employer',
+    image: '/images/coverage/general-lines.webp',
+  },
+  {
+    title: 'Event Cancellation',
+    bage1: 'Contingency',
+    bage2: 'Suspension',
+    image: '/images/coverage/event-cancellation.webp',
+  },
+  {
+    title: 'Medical Malpractice',
+    bage1: 'Healthcare',
+    bage2: 'Liability',
+    image: '/images/coverage/medical-malpractice.webp',
+  },
+  {
+    title: 'Political Violence',
+    bage1: 'Terrorism',
+    bage2: 'War',
+    image: '/images/coverage/political-violence.webp',
+  },
+  {
+    title: 'Kidnap & Ransom',
+    bage1: 'Extortion',
+    bage2: 'Ransom',
+    image: '/images/coverage/kidnap-ransom.webp',
+  },
+];
+
+export const advantagesCardListData = [
+  {
+    icon: Dashboard,
+    title: 'Digital Management',
+    text: 'Submit, track, and finalize risks entirely online.',
+  },
+  {
+    icon: Managment,
+    title: 'Automation & Efficiency',
+    text: 'Eliminate manual processes and paperwork.',
+  },
+  {
+    icon: Terms,
+    title: 'Adjustable Policy Terms',
+    text: 'Easily set and modify terms to meet your business needs.',
+  },
+  {
+    icon: Money,
+    title: 'Extensive Capacityy',
+    text: 'Access both onshore and DIFC-based reinsurers.',
+  },
+  {
+    icon: Dynamic,
+    title: 'Risk Summary',
+    text: 'Understand risk profiles and prioritize opportunities effectively.',
+  },
+  {
+    icon: Tracking,
+    title: 'Full Visibility & Reporting',
+    text: 'Track submissions, deals, and reports on your dashboard.',
+  },
+  {
+    icon: Distribution,
+    title: 'Reduce Distribution Fees',
+    text: 'Save up to 20% on intermediary and administrative costs.',
+  },
+  {
+    icon: Coverage,
+    title: 'Market Access',
+    text: 'Compare offers and track opportunities in real time.',
+  },
+  {
+    isAnimatedCard: true,
+    title: 'Get Started with Smarter Reinsurance.',
+  },
+];
+
+export const resultsCardListData = [
+  {
+    logo: SilverOakLogo,
+    label: 'SilverOak',
+    text: '“A leading UAE-based cedant placed $XXXM in reinsurance capacity across 4 Lines of Business using Surecede’s Digital Submission and Priority Matrix tools, reducing placement time by XX% and cutting distribution costs by XX%.”',
+  },
+  {
+    logo: NovaRELogo,
+    label: 'Nova RE',
+    text: '“A DIFC-based reinsurer reviewed over XXX targeted submissions within 3 months using Surecede’s API Integration and Analytics, growing their cedant network by XX% while reducing acquisition costs.”',
+  },
+  {
+    logo: ApexRiskLogo,
+    label: 'Apex Risk',
+    text: '“Through Surecede’s end-to-end digital facultative workflow, a KSA cedent and DIFC-based reinsurer closed a complex property reinsurance deal in 5 days, XX% faster than the market average.”',
+  },
+
+  // duplication
+
+  {
+    logo: SilverOakLogo,
+    label: 'SilverOak',
+    text: '“A leading UAE-based cedant placed $XXXM in reinsurance capacity across 4 Lines of Business using Surecede’s Digital Submission and Priority Matrix tools, reducing placement time by XX% and cutting distribution costs by XX%.”',
+  },
+  {
+    logo: NovaRELogo,
+    label: 'Nova RE',
+    text: '“A DIFC-based reinsurer reviewed over XXX targeted submissions within 3 months using Surecede’s API Integration and Analytics, growing their cedant network by XX% while reducing acquisition costs.”',
+  },
+  {
+    logo: ApexRiskLogo,
+    label: 'Apex Risk',
+    text: '“Through Surecede’s end-to-end digital facultative workflow, a KSA cedent and DIFC-based reinsurer closed a complex property reinsurance deal in 5 days, XX% faster than the market average.”',
+  },
+];
--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -2,16 +2,117 @@ import {
   FAQSection,
   StreamlinedReinsuranceSection,
   SureCedeImpactSection,
+  SliderCommon,
+  BenefitCardList,
+  BookDemoButton,
 } from '@/components/common';
+import { CoverageCard, ResultsCard } from '@/components/ui';
+
+import { useBreakpoint } from '@/hooks';
+
+import {
+  coverageDataCardList,
+  advantagesCardListData,
+  resultsCardListData,
+} from '@/data/homePageData';
 
 import './HomePage.scss';
 
 export default function HomePage() {
+  const { isMobile, isTablet, isLaptop } = useBreakpoint();
+
+  let peek;
+
+  switch (true) {
+    case isMobile:
+      peek = 310;
+      break;
+    case isTablet:
+      peek = 300;
+      break;
+    case isLaptop:
+      peek = 230;
+      break;
+
+    default:
+      peek = 163;
+
+      break;
+  }
+
   return (
     <>
       <SureCedeImpactSection />
+
+      <section className="section-general coverage-section">
+        <div className="container-general coverage-section__container">
+          <h3>Coverage Across Multiple Classes</h3>
+        </div>
+
+        <SliderCommon
+          cardComponent={CoverageCard}
+          dataList={coverageDataCardList}
+          spaceBetween={16}
+          peek={peek}
+        />
+      </section>
+
+      <section className="section-advantages">
+        <div className="container-general">
+          <div className="inner-container inner-container-with-borders">
+            <div className="section-advantages__header">
+              <h3>Why Choose Surecede</h3>
+
+              <p>
+                In the MENA reinsurance market, high intermediary fees up to 25%, fragmented
+                workflows, and lack of transparency slow deal-making. Surecede replaces these
+                inefficiencies with a fully digital, cost-effective, and transparent platform.
+              </p>
+            </div>
+
+            <BenefitCardList
+              cardListData={advantagesCardListData}
+              additionalClass="section-advantages__card-list"
+            />
+          </div>
+        </div>
+      </section>
+
       <StreamlinedReinsuranceSection />
+
+      <section className="section-general home-page__results-section">
+        <div className="container-general">
+          <h3>Real Results from Leading Cedants and Reinsurers</h3>
+        </div>
+
+        <SliderCommon
+          cardComponent={ResultsCard}
+          dataList={resultsCardListData}
+          spaceBetween={16}
+          width={isMobile ? '343px' : '540px'}
+        />
+      </section>
+
       <FAQSection />
+
+      <section className="home-page__bottom-section">
+        <div className="container-general">
+          <div className="inner-container-bottom inner-container-with-borders home-page__bottom-section__container">
+            <div className="home-page__bottom-section__header">
+              <h2>
+                Trusted by Cedants and <br /> Reinsurers Across MENA
+              </h2>
+
+              <p>
+                Join industry professionals streamlining reinsurance placement, <br /> accessing
+                capacity, and gaining market transparency.
+              </p>
+            </div>
+
+            <BookDemoButton />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
