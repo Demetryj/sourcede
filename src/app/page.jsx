840a4bdef6ecb092f447e29860d3455247a5b9fa
Demetryj <sdm17@ukr.net>
2025-10-25 00:49:59 +0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,14 +2,6 @@
 
 import { useEffect } from 'react';
 
-import {
-  PrimaryButton,
-  SecondaryButton,
-  TertiaryButton,
-  TextButton,
-  SubscriptionForm,
-  InputGeneral,
-} from '@/components/common';
 import { HomePage } from '@/views';
 
 export default function Home() {
@@ -17,20 +9,5 @@ export default function Home() {
     document.title = 'Home';
   });
 
-  return (
-    <>
-      <div>
-        <h2>asdasdasdad</h2>
-        <p>ashdkasdkjahksjdhaksjdhaks</p>
-        <TextButton>Platform</TextButton>
-        <TertiaryButton>Book a Demo</TertiaryButton>
-        <SecondaryButton>Book a Dem</SecondaryButton>
-        <PrimaryButton withIcon>Book a Demo</PrimaryButton>
-        <SubscriptionForm />
-        <InputGeneral name="sdsfsdf" />
-      </div>
-
-      <HomePage />
-    </>
-  );
+  return <HomePage />;
 }
