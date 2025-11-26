03480475dcf9ccebe9979bcbbe91175947ad806e
Demetryj <sdm17@ukr.net>
2025-11-26 16:03:40 +0200

    fix animations for Safari

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -44,7 +44,7 @@ export default function HomePage() {
 
   return (
     <>
-      {/* <HeroHomePageMobile /> */}
+      <HeroHomePageMobile />
       <HeroFlow />
 
       <SureCedeImpactSection />
